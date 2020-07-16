fc6d4534e2c9c2c19f16794fdee14435e97e383c
Sergi Botargues <sergibotargues@MacBook-Air-de-Sergi.local>
2020-07-16 17:56:01 +0200

    deploy (final -)

--- scripts/bbApiService.js
+++ scripts/bbApiService.js
@@ -10,11 +10,11 @@ function getCharacters() {
         .map(
           (data) =>
             ` <img class="images" src="${data.img}" alt="${data.name}"/>
-        <h4>Nickname: ${data.nickname}</h3>
-        <h4>Name: ${data.name}</h3>
-        <h4>Occupation: ${data.occupation}</h3>
-        <h4>Appearance: ${data.appearance}</h3>
-        <h4>Portrayed: ${data.portrayed}</h3>
+        <h4>Nickname: ${data.nickname}</h4>
+        <h4>Name: ${data.name}</h4>
+        <h4>Occupation: ${data.occupation}</h4>
+        <h4>Appearance: ${data.appearance}</h4>
+        <h4>Portrayed: ${data.portrayed}</h4>
         `
         )
         .join("");
